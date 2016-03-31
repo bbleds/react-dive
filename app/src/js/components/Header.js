74d07ab615cf0d705b0110501165e1319a9cb478
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-31 16:33:13 -0500

    Added basic functionality with state and props

--- app/src/js/components/Header.js
+++ app/src/js/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default class Header extends React.Component {
+
+  handleChange(e){
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+
+
   render(){
     return(
       <div>
         <h1>{this.props.title}</h1>
+        <input onChange={this.handleChange.bind(this)}/>
       </div>
     );
   }
--- app/src/js/components/Layout.js
+++ app/src/js/components/Layout.js
@@ -5,14 +5,19 @@ import Header from "./Header";
 export default class Layout extends React.Component {
   constructor() {
     super();
-    this.state = {name: "Ben"};
+    this.state = {
+      title: "Ben"
+    };
+  }
+
+  changeTitle(title){
+    this.setState({"title":title});
   }
 
   render(){
-    const title = "Welcome Ben";
     return(
       <div>
-        <Header title ={title}/>
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
         <Footer />
       </div>
     );
